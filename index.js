842c512841dc27bee73a9a738b45e96df0ffeecd
Joe Ippolite <ippolitejoe@gmail.com>
2015-06-27 00:23:16 -0400

    updated for depth sensor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var exec = require('child_process').exec;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -30,20 +31,16 @@ var serialport = new SerialPort("/dev/ttyACM0", {
 serialport.on('open', function(){
   console.log('Serial Port Opend');
   serialport.on('data', function(data){
-      io.emit('chat message', data);
-      console.log(data);
+      io.emit('arduino', data);
+      //console.log(data);
   });
 });
 
 //for testing, we're just going to send data to the client every second
 setInterval( function() {
 
-  /*
-    our message we want to send to the client: in this case it's just a random
-    number that we generate on the server
-  */
-  var msg = Math.random();
-  io.emit('other', msg);
-  console.log (msg);
+  exec("python ~/NEMO/python/PS-42_python/mav_reader.py", function(error, stdout, stderr){
+  	io.emit('mav', stdout);
+  });
 
 }, 1000);
