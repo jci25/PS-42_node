b29b3b4cd4bcef99de54a585ccc57ad5315fb626
Joe Ippolite <ippolitejoe@gmail.com>
2015-07-16 03:25:13 -0400

    updated for server communication when using vpn

--- index.js
+++ index.js
@@ -2,53 +2,28 @@ var exec = require('child_process').exec;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var SerialPort = require("serialport").SerialPort;
-var serialport = require("serialport");
-// list serial ports:
-serialport.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-  });
+var socket = require('socket.io-client')('http://10.8.0.6:8888');
+
+socket.on('connect', function(){
+   console.log("connect");
+});
+socket.on('arduino', function(data){
+	io.emit('arduino', data);
+});
+socket.on('mav', function(data){
+        io.emit('mav', data);
 });
 
+
+
 io.on('connection', function(socket){
-//io.emit('chat message', 'HEY');
+io.emit('chat message', 'HEY');
   socket.on('chat message', function(msg){
     //io.emit('chat message', msg);
     console.log(msg);
-  });
-  socket.on('getTables', function(msg){
-    io.emit('getTables', msg);
-  });
-  socket.on('table_names', function(msg){
-    console.log(msg);
-    io.emit('table_names', msg);
   });
 });
 
 http.listen(8888, function(){
   console.log('listening on *:8888');
 });
-
-var serialport = new SerialPort("/dev/ttyACM0", {
-   baudRate: 9600,
-   // look for return and newline at the end of each data packet:
-   parser: serialport.parsers.readline("\r\n")
- });
-serialport.on('open', function(){
-  console.log('Serial Port Opend');
-  serialport.on('data', function(data){
-      console.log(data);
-      io.emit('arduino', data);
-      console.log(data);
-  });
-});
-
-//for testing, we're just going to send data to the client every second
-setInterval( function() {
-
-  exec("python /home/pi/NEMO/python/PS-42_python/mav_reader.py", function(error, stdout, stderr){
-  	io.emit('mav', stdout);
-  });
-
-}, 2400);
